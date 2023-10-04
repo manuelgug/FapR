5c95663f1f0193393d2b6348b67cb2d0cc01f1a8
manuelgug <36465310+manuelgug@users.noreply.github.com>
2023-10-04 11:36:32 +0200

    Update exploring_new_concepts.R

--- exploring_new_concepts.R
+++ exploring_new_concepts.R
@@ -1,16 +1,19 @@
 
+
+###ESTE MÉTODO ITERATIVO IGNORA COI. USANDO COI SERÍA UNA ESTRATEGIA DISTINTA, SUPONGO... PROBAR ASÍ CON TODAS LAS ITERACIONES POSIBLES PRIMERO?
+
 #4 amplicons, all biallelic
 
 rm(a1, a2, a3, a4)
 
-a1 <- data.frame(dhps_437 = c("A", "G", "I", "Y"), 
-                 freq = c(0.58, 0.32, 0.09, 0.1))
-a2 <- data.frame(dhps_540 = c("K", "E", "O"), 
-                 freq = c(0.45, 0.37, 0.18))
-a3 <-data.frame(dhfr_51_59 = c("IR", "NC", "XY"), 
-                 freq = c(0.82, 0.16, 0.02))
-a4 <- data.frame(dhfr_108 = c("N", "S"), 
-                 freq = c(0.90, 0.1))
+a1 <- data.frame(dhps_437 = c("A", "G"), 
+                 freq = c(0.75, 0.25))
+a2 <- data.frame(dhps_540 = c("K"), 
+                 freq = c(1))
+a3 <-data.frame(dhfr_51_59 = c("IR", "NC"), 
+                 freq = c(0.84, 0.16))
+a4 <- data.frame(dhfr_108 = c("N"), 
+                 freq = c(1))
 
 
 #create all possible combinations of alleles and freqs
@@ -122,9 +125,72 @@ MOST_LIKELY_HAPLOS_FREQS <- rbind(MOST_LIKELY_HAPLOS_FREQS, temp )
 #################################################################3
 
 ##asumiendo sólo un haplo claramente most likely... seeguir:
+##asumiendo sólo un haplo claramente most likely... seeguir:
+
+#select minimum allele freq of most likely haplotype
+min_allele_from_most_lilely_hap <- min(modified_comb_freqs_matrix[highest_prob,1:4])
+
+#boolean mask to detect alleles that are present on the most likely haplotype
+row_to_match <- as.matrix(comb_alleles_matrix[highest_prob, ])
+
+mask <- sapply(colnames(comb_alleles_matrix), function(col_name) {
+  comb_alleles_matrix[, col_name] == row_to_match[,col_name]
+})
 
 
-###ESTE MÉTODO ITERATIVO IGNORA COI. USANDO COI SERÍA UNA ESTRATEGIA DISTINTA, SUPONGO... PROBAR ASÍ CON TODAS LAS ITERACIONES POSIBLES PRIMERO?
+modified_comb_freqs_matrix <- modified_comb_freqs_matrix[,1:4]
+
+# Subtract min_allele_from_most_lilely_hap from the cells where mask is TRUE and ignore the specified column
+modified_comb_freqs_matrix[mask] <-  modified_comb_freqs_matrix[mask] - min_allele_from_most_lilely_hap
+
+
+
+#NEXT ITERATION(?)
+modified_comb_freqs_matrix$freq_mean <- rowMeans(modified_comb_freqs_matrix, na.rm = TRUE) #calculate mean freq for all possible haplotypes
+modified_comb_freqs_matrix$probs<-modified_comb_freqs_matrix$dhps_437 * modified_comb_freqs_matrix$dhps_540 * modified_comb_freqs_matrix$dhfr_51_59 *modified_comb_freqs_matrix$dhfr_108 #calculate probs if all haploptypes were present
+modified_comb_freqs_matrix$SD <- apply(modified_comb_freqs_matrix[, 1:4], 1, sd) # Calculate Standard Deviation (SD)
+modified_comb_freqs_matrix$CV <- (modified_comb_freqs_matrix$SD / modified_comb_freqs_matrix$freq_mean) # Calculate Coefficient of Variation (CV)
+
+#plot shit
+plot(modified_comb_freqs_matrix$CV, modified_comb_freqs_matrix$probs)
+
+## select the "BEST" haplo: highest prob and lowest CV
+lowest_CV <- which.min(modified_comb_freqs_matrix$CV)
+highest_prob <- as.numeric(which.max(modified_comb_freqs_matrix$probs))
+
+if (lowest_CV == highest_prob){
+  
+  most_likely_hap<-paste(as.matrix(comb_alleles_matrix[highest_prob, ]), collapse="_")
+  print(paste( most_likely_hap, "is the most likely true haplotype.", collapse=" "))
+  
+}else{
+  
+  most_likely_hap1<-paste(as.matrix(comb_alleles_matrix[highest_prob, ]), collapse="_")
+  most_likely_hap2<-paste(as.matrix(comb_alleles_matrix[lowest_CV, ]), collapse="_")
+  print(paste("One of", most_likely_hap1, "and", most_likely_hap2, "is the most likely true haplotype. Visually examine the plot."))
+  
+  plot(comb_freqs_matrix$CV, comb_freqs_matrix$probs)
+  
+  ##BREAK??
+}
+
+#append next best haplo:
+MOST_LIKELY_HAPLOS <- rbind(MOST_LIKELY_HAPLOS,comb_alleles_matrix[highest_prob, ])
+temp <-modified_comb_freqs_matrix[highest_prob, ]
+temp$HAPLO_FREQ <- min(modified_comb_freqs_matrix[highest_prob,1:4])
+MOST_LIKELY_HAPLOS_FREQS <- rbind(MOST_LIKELY_HAPLOS_FREQS, temp )
+
+### DONE, CLEAN! poner chequeo: si current SD = 0, END.
+
+
+
+
+
+
+
+
+
+
 
 
 

2117707e3394896086e98c452261cfff0075b593
manuelgug <36465310+manuelgug@users.noreply.github.com>
2023-10-06 10:21:25 +0200

    main loop for mathematical approach DONE

--- exploring_new_concepts.R
+++ exploring_new_concepts.R
@@ -2,6 +2,10 @@
 
 ###ESTE MÉTODO ITERATIVO IGNORA COI. USANDO COI SERÍA UNA ESTRATEGIA DISTINTA, SUPONGO... PROBAR ASÍ CON TODAS LAS ITERACIONES POSIBLES PRIMERO?
 
+
+
+################## IMPORT AND FORMAT DATA ################## 
+
 #4 amplicons, all biallelic
 
 rm(a1, a2, a3, a4)
@@ -28,163 +32,68 @@ colnames(comb_alleles_matrix) <- c("dhps_437", "dhps_540", "dhfr_51_59", "dhfr_1
 comb_freqs_matrix <- as.data.frame(comb_freqs)
 colnames(comb_freqs_matrix) <- c("dhps_437", "dhps_540", "dhfr_51_59", "dhfr_108")
 
-comb_freqs_matrix$freq_mean <- rowMeans(comb_freqs_matrix, na.rm = TRUE) #calculate mean freq for all possible haplotypes
-comb_freqs_matrix$probs<-comb_freqs_matrix$dhps_437 * comb_freqs_matrix$dhps_540 * comb_freqs_matrix$dhfr_51_59 *comb_freqs_matrix$dhfr_108 #calculate probs if all haploptypes were present
-comb_freqs_matrix$SD <- apply(comb_freqs_matrix[, 1:4], 1, sd) # Calculate Standard Deviation (SD)
-comb_freqs_matrix$CV <- (comb_freqs_matrix$SD / comb_freqs_matrix$freq_mean) # Calculate Coefficient of Variation (CV)
 
-#plot shit
-plot(comb_freqs_matrix$CV, comb_freqs_matrix$probs)
+################## MAIN LOOP ################## 
 
-## select the "BEST" haplo: highest prob and lowest CV
-lowest_CV <- which.min(comb_freqs_matrix$CV)
-highest_prob <- as.numeric(which.max(comb_freqs_matrix$probs))
+MOST_LIKELY_HAPLOS <- data.frame()
+MOST_LIKELY_HAPLOS_FREQS <- data.frame()
 
-if (lowest_CV == highest_prob){
+
+while (dim(MOST_LIKELY_HAPLOS_FREQS)[1] == 0 || sd(MOST_LIKELY_HAPLOS_FREQS[nrow(MOST_LIKELY_HAPLOS_FREQS), 1:4]) > 0.00000000001) {
+  # Calculate mean freq for all possible haplotypes
+  comb_freqs_matrix$freq_mean <- rowMeans(comb_freqs_matrix, na.rm = TRUE)
   
-  most_likely_hap<-paste(as.matrix(comb_alleles_matrix[highest_prob, ]), collapse="_")
-  print(paste( most_likely_hap, "is the most likely true haplotype.", collapse=" "))
+  # Calculate probs if all haplotypes were present
+  comb_freqs_matrix$probs <- comb_freqs_matrix$dhps_437 * comb_freqs_matrix$dhps_540 * comb_freqs_matrix$dhfr_51_59 * comb_freqs_matrix$dhfr_108
   
-}else{
+  # Calculate Standard Deviation (SD)
+  comb_freqs_matrix$SD <- apply(comb_freqs_matrix[, 1:4], 1, sd)
   
-  most_likely_hap1<-paste(as.matrix(comb_alleles_matrix[highest_prob, ]), collapse="_")
-  most_likely_hap2<-paste(as.matrix(comb_alleles_matrix[lowest_CV, ]), collapse="_")
-  print(paste("One of", most_likely_hap1, "and", most_likely_hap2, "is the most likely true haplotype. Visually examine the plot."))
+  # Calculate Coefficient of Variation (CV)
+  comb_freqs_matrix$CV <- (comb_freqs_matrix$SD / comb_freqs_matrix$freq_mean)
   
-  plot(comb_freqs_matrix$CV, comb_freqs_matrix$probs)
+  ## Select the "BEST" haplo: highest prob and lowest CV
+  lowest_CV <- which.min(comb_freqs_matrix$CV)
+  highest_prob <- as.numeric(which.max(comb_freqs_matrix$probs))
   
-  ##BREAK??
-}
-
-#store most likely haplo1 in new dfs:
-
-MOST_LIKELY_HAPLOS <- comb_alleles_matrix[highest_prob, ]
-MOST_LIKELY_HAPLOS_FREQS <- comb_freqs_matrix[highest_prob, ]
-MOST_LIKELY_HAPLOS_FREQS$HAPLO_FREQ<-min(comb_freqs_matrix[highest_prob,1:4])
-
-
-
-#################################################################3
-
-##asumiendo sólo un haplo claramente most likely... seeguir:
-
-#select minimum allele freq of most likely haplotype
-min_allele_from_most_lilely_hap <- min(comb_freqs_matrix[highest_prob,1:4])
-
-#boolean mask to detect alleles that are present on the most likely haplotype
-row_to_match <- as.matrix(comb_alleles_matrix[highest_prob, ])
-
+  if (lowest_CV == highest_prob) {
+    most_likely_hap <- paste(as.matrix(comb_alleles_matrix[highest_prob, ]), collapse = "_")
+    print(paste(most_likely_hap, "is the most likely true haplotype.", collapse = " "))
+  } else {
+    most_likely_hap1 <- paste(as.matrix(comb_alleles_matrix[highest_prob, ]), collapse = "_")
+    most_likely_hap2 <- paste(as.matrix(comb_alleles_matrix[lowest_CV, ]), collapse = "_")
+    print(paste("One of", most_likely_hap1, "and", most_likely_hap2, "is the most likely true haplotype. Visually examine the plot."))
+    
+    # Plot CV vs. probs
+    plot(comb_freqs_matrix$CV, comb_freqs_matrix$probs)
+  }
+  
+  # Append next best haplo
+  MOST_LIKELY_HAPLOS <- rbind(MOST_LIKELY_HAPLOS, comb_alleles_matrix[highest_prob, ])
+  temp <- comb_freqs_matrix[highest_prob, ]
+  temp$HAPLO_FREQ <- min(comb_freqs_matrix[highest_prob, 1:4])
+  MOST_LIKELY_HAPLOS_FREQS <- rbind(MOST_LIKELY_HAPLOS_FREQS, temp)
+  
+  # Select minimum allele freq of the most likely haplotype
+  min_allele_from_most_lilely_hap <- min(comb_freqs_matrix[highest_prob, 1:4])
+  
+  # Boolean mask to detect alleles that are present on the most likely haplotype
+  row_to_match <- as.matrix(comb_alleles_matrix[highest_prob, ])
   mask <- sapply(colnames(comb_alleles_matrix), function(col_name) {
-  comb_alleles_matrix[, col_name] == row_to_match[,col_name]
-})
-
-
-modified_comb_freqs_matrix <- comb_freqs_matrix[,1:4]
+    comb_alleles_matrix[, col_name] == row_to_match[, col_name]
+  })
   
-# Subtract min_allele_from_most_lilely_hap from the cells where mask is TRUE and ignore the specified column
-modified_comb_freqs_matrix[mask] <-  modified_comb_freqs_matrix[mask] - min_allele_from_most_lilely_hap
-
-
-
-#NEXT ITERATION(?)
-modified_comb_freqs_matrix$freq_mean <- rowMeans(modified_comb_freqs_matrix, na.rm = TRUE) #calculate mean freq for all possible haplotypes
-modified_comb_freqs_matrix$probs<-modified_comb_freqs_matrix$dhps_437 * modified_comb_freqs_matrix$dhps_540 * modified_comb_freqs_matrix$dhfr_51_59 *modified_comb_freqs_matrix$dhfr_108 #calculate probs if all haploptypes were present
-modified_comb_freqs_matrix$SD <- apply(modified_comb_freqs_matrix[, 1:4], 1, sd) # Calculate Standard Deviation (SD)
-modified_comb_freqs_matrix$CV <- (modified_comb_freqs_matrix$SD / modified_comb_freqs_matrix$freq_mean) # Calculate Coefficient of Variation (CV)
-
-#plot shit
-plot(modified_comb_freqs_matrix$CV, modified_comb_freqs_matrix$probs)
-
-## select the "BEST" haplo: highest prob and lowest CV
-lowest_CV <- which.min(modified_comb_freqs_matrix$CV)
-highest_prob <- as.numeric(which.max(modified_comb_freqs_matrix$probs))
-
-if (lowest_CV == highest_prob){
+  comb_freqs_matrix <- comb_freqs_matrix[, 1:4]
   
-  most_likely_hap<-paste(as.matrix(comb_alleles_matrix[highest_prob, ]), collapse="_")
-  print(paste( most_likely_hap, "is the most likely true haplotype.", collapse=" "))
-  
-}else{
-  
-  most_likely_hap1<-paste(as.matrix(comb_alleles_matrix[highest_prob, ]), collapse="_")
-  most_likely_hap2<-paste(as.matrix(comb_alleles_matrix[lowest_CV, ]), collapse="_")
-  print(paste("One of", most_likely_hap1, "and", most_likely_hap2, "is the most likely true haplotype. Visually examine the plot."))
-  
-  plot(comb_freqs_matrix$CV, comb_freqs_matrix$probs)
-  
-  ##BREAK??
+  # Subtract min_allele_from_most_lilely_hap from the cells where mask is TRUE and ignore the specified column
+  comb_freqs_matrix[mask] <- comb_freqs_matrix[mask] - min_allele_from_most_lilely_hap
 }
 
-#append next best haplo:
-MOST_LIKELY_HAPLOS <- rbind(MOST_LIKELY_HAPLOS,comb_alleles_matrix[highest_prob, ])
-temp <-modified_comb_freqs_matrix[highest_prob, ]
-temp$HAPLO_FREQ <- min(modified_comb_freqs_matrix[highest_prob,1:4])
-MOST_LIKELY_HAPLOS_FREQS <- rbind(MOST_LIKELY_HAPLOS_FREQS, temp )
+#########################################################################################
 
 
-#################################################################3
-
-##asumiendo sólo un haplo claramente most likely... seeguir:
-##asumiendo sólo un haplo claramente most likely... seeguir:
-
-#select minimum allele freq of most likely haplotype
-min_allele_from_most_lilely_hap <- min(modified_comb_freqs_matrix[highest_prob,1:4])
-
-#boolean mask to detect alleles that are present on the most likely haplotype
-row_to_match <- as.matrix(comb_alleles_matrix[highest_prob, ])
-
-mask <- sapply(colnames(comb_alleles_matrix), function(col_name) {
-  comb_alleles_matrix[, col_name] == row_to_match[,col_name]
-})
-
-
-modified_comb_freqs_matrix <- modified_comb_freqs_matrix[,1:4]
-
-# Subtract min_allele_from_most_lilely_hap from the cells where mask is TRUE and ignore the specified column
-modified_comb_freqs_matrix[mask] <-  modified_comb_freqs_matrix[mask] - min_allele_from_most_lilely_hap
-
-
-
-#NEXT ITERATION(?)
-modified_comb_freqs_matrix$freq_mean <- rowMeans(modified_comb_freqs_matrix, na.rm = TRUE) #calculate mean freq for all possible haplotypes
-modified_comb_freqs_matrix$probs<-modified_comb_freqs_matrix$dhps_437 * modified_comb_freqs_matrix$dhps_540 * modified_comb_freqs_matrix$dhfr_51_59 *modified_comb_freqs_matrix$dhfr_108 #calculate probs if all haploptypes were present
-modified_comb_freqs_matrix$SD <- apply(modified_comb_freqs_matrix[, 1:4], 1, sd) # Calculate Standard Deviation (SD)
-modified_comb_freqs_matrix$CV <- (modified_comb_freqs_matrix$SD / modified_comb_freqs_matrix$freq_mean) # Calculate Coefficient of Variation (CV)
-
-#plot shit
-plot(modified_comb_freqs_matrix$CV, modified_comb_freqs_matrix$probs)
-
-## select the "BEST" haplo: highest prob and lowest CV
-lowest_CV <- which.min(modified_comb_freqs_matrix$CV)
-highest_prob <- as.numeric(which.max(modified_comb_freqs_matrix$probs))
-
-if (lowest_CV == highest_prob){
-  
-  most_likely_hap<-paste(as.matrix(comb_alleles_matrix[highest_prob, ]), collapse="_")
-  print(paste( most_likely_hap, "is the most likely true haplotype.", collapse=" "))
-  
-}else{
-  
-  most_likely_hap1<-paste(as.matrix(comb_alleles_matrix[highest_prob, ]), collapse="_")
-  most_likely_hap2<-paste(as.matrix(comb_alleles_matrix[lowest_CV, ]), collapse="_")
-  print(paste("One of", most_likely_hap1, "and", most_likely_hap2, "is the most likely true haplotype. Visually examine the plot."))
-  
-  plot(comb_freqs_matrix$CV, comb_freqs_matrix$probs)
-  
-  ##BREAK??
-}
-
-#append next best haplo:
-MOST_LIKELY_HAPLOS <- rbind(MOST_LIKELY_HAPLOS,comb_alleles_matrix[highest_prob, ])
-temp <-modified_comb_freqs_matrix[highest_prob, ]
-temp$HAPLO_FREQ <- min(modified_comb_freqs_matrix[highest_prob,1:4])
-MOST_LIKELY_HAPLOS_FREQS <- rbind(MOST_LIKELY_HAPLOS_FREQS, temp )
-
-### DONE, CLEAN! poner chequeo: si current SD = 0, END.
-
-
-
-
+MOST_LIKELY_HAPLOS
+MOST_LIKELY_HAPLOS_FREQS
 
 
 

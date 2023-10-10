b32ec16bbb8c1c31ae0b252f77eb6d690efb8692
manuelgug <36465310+manuelgug@users.noreply.github.com>
2023-10-10 09:53:57 +0200

    Put back HAPLO_FREQ_RECALC column

--- exploring_new_concepts.R
+++ exploring_new_concepts.R
@@ -20,7 +20,7 @@ moire_output <- read.csv("moire_output.csv")
 ################## MAIN LOOP ################## 
 
 unique_samples <- unique(resmarkers_table$SampleID)
-RESULTS_FINAL <- data.frame(SampleID = character(0), dhps_437 = character(0), dhps_540 = character(0), dhfr_51 = character(0), dhfr_59 = character(0), dhfr_108 = character(0), HAPLO_FREQ = numeric(0))#, HAPLO_FREQ_RECALC = numeric(0))
+RESULTS_FINAL <- data.frame(SampleID = character(0), dhps_437 = character(0), dhps_540 = character(0), dhfr_51 = character(0), dhfr_59 = character(0), dhfr_108 = character(0), HAPLO_FREQ = numeric(0), HAPLO_FREQ_RECALC = numeric(0))
 
 # INIT LOOP HERE!
 for (sample in unique_samples){
@@ -30,13 +30,13 @@ for (sample in unique_samples){
   eCOI_counter <- 0
   MOST_LIKELY_HAPLOS <- data.frame()
   MOST_LIKELY_HAPLOS_FREQS <- data.frame()
-  RESULTS <- data.frame(SampleID = character(0), dhps_437 = character(0), dhps_540 = character(0), dhfr_51 = character(0), dhfr_59 = character(0), dhfr_108 = character(0), HAPLO_FREQ = numeric(0))#, HAPLO_FREQ_RECALC = numeric(0))
+  RESULTS <- data.frame(SampleID = character(0), dhps_437 = character(0), dhps_540 = character(0), dhfr_51 = character(0), dhfr_59 = character(0), dhfr_108 = character(0), HAPLO_FREQ = numeric(0), HAPLO_FREQ_RECALC = numeric(0))
   
   # 1) select sample
   sID <- resmarkers_table[resmarkers_table$SampleID == sample,]
   
   # 2) select sample's eCOI
-  eCOI<- moire_output[moire_output$sample_id == sample,][2] #now set to POST_COI_LOWER because makes sense with visual inspection of controls. however, need more testing
+  eCOI<- moire_output[moire_output$sample_id == sample,][2] #now set to POST_COI_LOWER because makes sense with visual inspection of controls. however, needs more testing
   
   # 3) format data
   new_df <- data.frame(matrix(ncol = length(sID$resmarker), nrow=1))
@@ -81,7 +81,7 @@ for (sample in unique_samples){
   # 4) phase
   if (dim(comb_alleles_matrix)[1] != 1){ #basically, don't process monoallelic samples 'cause they make the loop crash
     
-    while (dim(MOST_LIKELY_HAPLOS_FREQS)[1] == 0 || eCOI_counter <= eCOI && sum(unique(comb_freqs_matrix)) > 1E-4) { #### AQUÍ VOY! TENGO PROBLEMAS CON LA CONDICIÓN SI COI PERMITE MUCHAS ITERACIONES
+    while (dim(MOST_LIKELY_HAPLOS_FREQS)[1] == 0 || eCOI_counter <= eCOI && sum(unique(comb_freqs_matrix)) > 1E-4) { ## PULIR CONDICIÓN DE SUM(UNIQUE)
       
       eCOI_counter <- eCOI_counter + 1
       
@@ -114,7 +114,7 @@ for (sample in unique_samples){
       MOST_LIKELY_HAPLOS <- rbind(MOST_LIKELY_HAPLOS, comb_alleles_matrix[highest_prob, ])
       temp <- comb_freqs_matrix[highest_prob, ]
       temp$HAPLO_FREQ <- min(comb_freqs_matrix[highest_prob, 1:5])
-      #temp$HAPLO_FREQ_RECALC <- NA
+      temp$HAPLO_FREQ_RECALC <- NA
       MOST_LIKELY_HAPLOS_FREQS <- rbind(MOST_LIKELY_HAPLOS_FREQS, temp)
       
       # Select minimum allele freq from the most likely haplotype
@@ -131,15 +131,15 @@ for (sample in unique_samples){
       comb_freqs_matrix[mask] <- comb_freqs_matrix[mask] - min_allele_from_most_lilely_hap
       
       #recalculate proportions of final haplos
-      #MOST_LIKELY_HAPLOS_FREQS$HAPLO_FREQ_RECALC <- MOST_LIKELY_HAPLOS_FREQS$HAPLO_FREQ / sum(MOST_LIKELY_HAPLOS_FREQS$HAPLO_FREQ)
+      MOST_LIKELY_HAPLOS_FREQS$HAPLO_FREQ_RECALC <- MOST_LIKELY_HAPLOS_FREQS$HAPLO_FREQ / sum(MOST_LIKELY_HAPLOS_FREQS$HAPLO_FREQ)
       
-      RESULTS <- cbind(SampleID = sample, MOST_LIKELY_HAPLOS, HAPLO_FREQ = MOST_LIKELY_HAPLOS_FREQS$HAPLO_FREQ)#, HAPLO_FREQ_RECALC = MOST_LIKELY_HAPLOS_FREQS$HAPLO_FREQ_RECALC)
+      RESULTS <- cbind(SampleID = sample, MOST_LIKELY_HAPLOS, HAPLO_FREQ = MOST_LIKELY_HAPLOS_FREQS$HAPLO_FREQ, HAPLO_FREQ_RECALC = MOST_LIKELY_HAPLOS_FREQS$HAPLO_FREQ_RECALC)
     }  
     
   }else{ 
     
     #FORMAT AND ADD MONOALLELIC SAMPLES HERE
-    RESULTS <- cbind(SampleID = sample, comb_alleles_matrix, HAPLO_FREQ = 1)#, HAPLO_FREQ_RECALC = 1)
+    RESULTS <- cbind(SampleID = sample, comb_alleles_matrix, HAPLO_FREQ = 1, HAPLO_FREQ_RECALC = 1)
   }
   
   #DONE

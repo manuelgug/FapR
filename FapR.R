3d3320c51d844d543da972b0a9d0116dcff7b617
manuelgug <36465310+manuelgug@users.noreply.github.com>
2023-10-25 09:58:27 +0200

    Added dhps_431 and dhps_581 markers

--- FapR.R
+++ FapR.R
@@ -5,9 +5,9 @@ library(optparse)
 
 # Define the command-line arguments
 option_list <- list(
-  make_option(c("--resmarkers_table", "-i"), type = "character", help = "FILTERED resmarkers table from mad4hatter v0.1.8", default = "../results_v0.1.8_RESMARKERS_FIX/HFS_22_03_RESULTS_v0.1.8_FILTERED/resmarker_table_global_max_0_filtered.csv"),
+  make_option(c("--resmarkers_table", "-i"), type = "character", help = "FILTERED resmarkers table from mad4hatter v0.1.8", default = "../results_v0.1.8_RESMARKERS_FIX/HFS_NextSeq01_RESULTS_v0.1.8_FILTERED/resmarker_table_global_max_0_filtered.csv"),
   make_option(c("--output_prefix", "-o"), type = "character", help = "Distinctive prefix for your output files", default = "test"),
-  make_option(c("--moire_output", "-m"), type = "character", help = "Moire output calculated from the dhfr-dhps amplicons", default = "HFS22_03_moire_output_dhfr_dhps.csv")
+  make_option(c("--moire_output", "-m"), type = "character", help = "Moire output calculated from the dhfr-dhps amplicons", default = "HFS22_01_moire_output_dhfr_dhps.csv")
 )
 
 # Parse the command-line arguments
@@ -23,7 +23,7 @@ if (is.null(opt$resmarkers_table) || is.null(opt$output_prefix) || is.null(opt$m
 resmarkers_table <- read.csv(opt$resmarkers_table)
 
 #subset relevant markers
-markers_to_phase <- c("dhfr_51", "dhfr_59", "dhfr_108", "dhps_437", "dhps_540")
+markers_to_phase <- c("dhfr_51", "dhfr_59", "dhfr_108", "dhps_431", "dhps_437", "dhps_540", "dhps_581")
 
 resmarkers_table <- resmarkers_table %>%
   filter(grepl(paste(markers_to_phase, collapse = "|"), resmarker))
@@ -36,7 +36,7 @@ moire_output <- read.csv(opt$moire_output) #dhfr-dhps specific moire run
 ################## MAIN LOOP ################## 
 
 unique_samples <- unique(resmarkers_table$SampleID)
-RESULTS_FINAL <- data.frame(SampleID = character(0), dhps_437 = character(0), dhps_540 = character(0), dhfr_51 = character(0), dhfr_59 = character(0), dhfr_108 = character(0), HAPLO_FREQ = numeric(0), HAPLO_FREQ_RECALC = numeric(0))
+RESULTS_FINAL <- data.frame(SampleID = character(0), dhps_431 = character(0), dhps_437 = character(0), dhps_540 = character(0), dhps_581 = character(0), dhfr_51 = character(0), dhfr_59 = character(0), dhfr_108 = character(0), HAPLO_FREQ = numeric(0), HAPLO_FREQ_RECALC = numeric(0))
 
 # INIT LOOP HERE!
 for (sample in unique_samples){
@@ -46,7 +46,7 @@ for (sample in unique_samples){
   COI_counter <- 0
   MOST_LIKELY_HAPLOS <- data.frame()
   MOST_LIKELY_HAPLOS_FREQS <- data.frame()
-  RESULTS <- data.frame(SampleID = character(0), dhps_437 = character(0), dhps_540 = character(0), dhfr_51 = character(0), dhfr_59 = character(0), dhfr_108 = character(0), HAPLO_FREQ = numeric(0), HAPLO_FREQ_RECALC = numeric(0))
+  RESULTS <- data.frame(SampleID = character(0), dhps_431 = character(0), dhps_437 = character(0), dhps_540 = character(0), dhps_581 = character(0), dhfr_51 = character(0), dhfr_59 = character(0), dhfr_108 = character(0), HAPLO_FREQ = numeric(0), HAPLO_FREQ_RECALC = numeric(0), HAPLO_FREQ = numeric(0), HAPLO_FREQ_RECALC = numeric(0))
   
   # 1) select sample
   sID <- resmarkers_table[resmarkers_table$SampleID == sample,]
@@ -74,14 +74,18 @@ for (sample in unique_samples){
     resulting_dataframes[[resmarker]] <- df
   }
   
-  alleles<-list(resulting_dataframes$dhps_437$dhps_437, 
+  alleles<-list(resulting_dataframes$dhps_431$dhps_431,
+                resulting_dataframes$dhps_437$dhps_437, 
                 resulting_dataframes$dhps_540$dhps_540, 
+                resulting_dataframes$dhps_581$dhps_581,
                 resulting_dataframes$dhfr_51$dhfr_51,
                 resulting_dataframes$dhfr_59$dhfr_59,
                 resulting_dataframes$dhfr_108$dhfr_108) #order is important
   
-  freqs<-list(resulting_dataframes$dhps_437$norm.reads.locus, 
+  freqs<-list(resulting_dataframes$dhps_431$norm.reads.locus,
+              resulting_dataframes$dhps_437$norm.reads.locus, 
               resulting_dataframes$dhps_540$norm.reads.locus, 
+              resulting_dataframes$dhps_581$norm.reads.locus,
               resulting_dataframes$dhfr_51$norm.reads.locus,
               resulting_dataframes$dhfr_59$norm.reads.locus,
               resulting_dataframes$dhfr_108$norm.reads.locus) #order is important
@@ -96,9 +100,9 @@ for (sample in unique_samples){
   }
   
   comb_alleles_matrix <- as.data.frame(comb_alleles)
-  colnames(comb_alleles_matrix) <- c("dhps_437", "dhps_540", "dhfr_51","dhfr_59", "dhfr_108")
+  colnames(comb_alleles_matrix) <- c("dhps_431", "dhps_437", "dhps_540", "dhps_581", "dhfr_51","dhfr_59", "dhfr_108")
   comb_freqs_matrix <- as.data.frame(comb_freqs)
-  colnames(comb_freqs_matrix) <- c("dhps_437", "dhps_540", "dhfr_51","dhfr_59", "dhfr_108")
+  colnames(comb_freqs_matrix) <- c("dhps_431", "dhps_437", "dhps_540", "dhps_581", "dhfr_51","dhfr_59", "dhfr_108")
   
   # 4) phase
   if (dim(comb_alleles_matrix)[1] != 1){ #basically, don't process monoallelic samples 'cause they make the loop crash
@@ -108,14 +112,14 @@ for (sample in unique_samples){
       COI_counter <- COI_counter + 1
       
       # Calculate probs if all haplotypes were present
-      comb_freqs_matrix$probs <- comb_freqs_matrix$dhps_437 * comb_freqs_matrix$dhps_540 * comb_freqs_matrix$dhfr_51 * comb_freqs_matrix$dhfr_59  * comb_freqs_matrix$dhfr_108
+      comb_freqs_matrix$probs <- comb_freqs_matrix$dhps_431 * comb_freqs_matrix$dhps_437 * comb_freqs_matrix$dhps_540 * comb_freqs_matrix$dhps_581 * comb_freqs_matrix$dhfr_51 * comb_freqs_matrix$dhfr_59  * comb_freqs_matrix$dhfr_108
       
       #remove haplotypes with prob = 0
       #comb_freqs_matrix <- subset(comb_freqs_matrix, probs != 0)
       
       # Calculate SD and CV
       comb_freqs_matrix$freq_mean <- rowMeans(comb_freqs_matrix, na.rm = TRUE)
-      comb_freqs_matrix$SD <- apply(comb_freqs_matrix[, 1:5], 1, sd)
+      comb_freqs_matrix$SD <- apply(comb_freqs_matrix[, 1:7], 1, sd)
       comb_freqs_matrix$CV <- (comb_freqs_matrix$SD / comb_freqs_matrix$freq_mean)
       
       ## Select the "BEST" haplo: highest prob and lowest CV
@@ -140,7 +144,7 @@ for (sample in unique_samples){
       MOST_LIKELY_HAPLOS_FREQS <- rbind(MOST_LIKELY_HAPLOS_FREQS, temp)
       
       # Select minimum allele freq from the most likely haplotype
-      min_allele_from_most_lilely_hap <- min(comb_freqs_matrix[highest_prob, 1:5])
+      min_allele_from_most_lilely_hap <- min(comb_freqs_matrix[highest_prob, 1:7])
       
       # Boolean mask to detect alleles that are present on the most likely haplotype
       row_to_match <- as.matrix(comb_alleles_matrix[highest_prob, ])
@@ -149,7 +153,7 @@ for (sample in unique_samples){
       })
       
       # Subtract min_allele_from_most_likely_hap from the cells where mask is TRUE and ignore the specified column
-      comb_freqs_matrix <- comb_freqs_matrix[, 1:5]
+      comb_freqs_matrix <- comb_freqs_matrix[, 1:7]
       comb_freqs_matrix[mask] <- comb_freqs_matrix[mask] - min_allele_from_most_lilely_hap
       
       #recalculate proportions of final haplos
@@ -204,7 +208,7 @@ write.csv(RESULTS_FINAL_FLAGGED_correct_only, paste0(opt$output_prefix, "_phased
 
 generate_haplo_summary_plots <- function(RESULTS_FINAL, props_plot, hist_plot, profile_plot) {
   # Formatting
-  haplos <- paste(RESULTS_FINAL$dhps_437, RESULTS_FINAL$dhps_540, RESULTS_FINAL$dhfr_51, RESULTS_FINAL$dhfr_59, RESULTS_FINAL$dhfr_108, sep = "_")
+  haplos <- paste(RESULTS_FINAL$dhps_431, RESULTS_FINAL$dhps_437, RESULTS_FINAL$dhps_540, RESULTS_FINAL$dhps_581, RESULTS_FINAL$dhfr_51, RESULTS_FINAL$dhfr_59, RESULTS_FINAL$dhfr_108, sep = "_")
   haplo_counts <- table(haplos)
   haplo_counts <- as.data.frame(haplo_counts)
   haplo_counts$haplos <- factor(haplo_counts$haplos, levels = haplo_counts$haplos[order(-haplo_counts$Freq)])
@@ -242,7 +246,7 @@ generate_haplo_summary_plots <- function(RESULTS_FINAL, props_plot, hist_plot, p
   grid_plot2 <- grid.arrange(grobs = histogram_plots, ncol = 4)
   
   # Stacked barplot per sample (only multiallelic)
-  RESULTS_FINAL$haplotype <- paste(RESULTS_FINAL$dhps_437, RESULTS_FINAL$dhps_540, RESULTS_FINAL$dhfr_51, RESULTS_FINAL$dhfr_59, RESULTS_FINAL$dhfr_108, sep = "_")
+  RESULTS_FINAL$haplotype <- paste(RESULTS_FINAL$dhps_431, RESULTS_FINAL$dhps_437, RESULTS_FINAL$dhps_540, RESULTS_FINAL$dhps_581, RESULTS_FINAL$dhfr_51, RESULTS_FINAL$dhfr_59, RESULTS_FINAL$dhfr_108, sep = "_")
   RESULTS_FINAL_multiallelic <- RESULTS_FINAL[RESULTS_FINAL$HAPLO_FREQ_RECALC < 1, ]
   
   a <- ggplot(RESULTS_FINAL_multiallelic, aes(x = SampleID, y = HAPLO_FREQ_RECALC, fill = haplotype)) +
@@ -272,7 +276,7 @@ generate_haplo_summary_plots(RESULTS_FINAL_FLAGGED_correct_only,
                              paste0(opt$output_prefix, "_haplo_profile_multiallelic_correct_only.png"))
 
 
-RESULTS_FINAL_FLAGGED_correct_only$Haplotype <- do.call(paste0, RESULTS_FINAL_FLAGGED_correct_only[, 2:6])
+RESULTS_FINAL_FLAGGED_correct_only$Haplotype <- do.call(paste0, RESULTS_FINAL_FLAGGED_correct_only[, 2:8])
 RESULTS_FINAL_FLAGGED_correct_only <- subset(RESULTS_FINAL_FLAGGED_correct_only, HAPLO_FREQ_RECALC != 1)
 
 RESULTS_FINAL_FLAGGED_correct_only <- RESULTS_FINAL_FLAGGED_correct_only %>%
